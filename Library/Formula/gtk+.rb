74f5f710ea8fcd93004e58bb24f50702c6e10b8e
J. Brandt Buckley <brandt@runlevel1.com>
2012-07-04 20:34:52 -0700

    gtk: made docs optional due to gtk-doc dep issue
    ISSUE: GTK+-2.24.10 requires the gtk-doc package which itself requires the
    libxml2 Python library. Consequently, gtk-doc fails.
    
    The current libxml2 formula has a '--with-python' option, but:
    
    1) There isn't currently a way for one formula to depend upon another
    formula with an option specified.
    
    2) Even with the Python module, you still have to either alter
    $PYTHONPATH or manually symlink the module into the expected
    site-packages in order for gtk-doc to compile.
    
    WORKAROUND:
    * Added a patch that prevents the docs from being compiled.
    * Added an option, '--with-docs' that turns off said patch, allowing docs
    to compile normally.

--- Library/Formula/gtk+.rb
+++ Library/Formula/gtk+.rb
@@ -12,16 +12,26 @@ class Gtkx < Formula
   depends_on 'gdk-pixbuf'
   depends_on 'pango'
   depends_on 'jasper' => :optional
-  depends_on 'atk' => :optional
+  depends_on 'atk'
   depends_on 'cairo'
   depends_on :x11
+  depends_on 'gtk-doc' if ARGV.include? '--with-docs'
 
   fails_with :llvm do
     build 2326
     cause "Undefined symbols when linking"
   end
-  
-  def patches    
+
+  def options
+  [
+    ['--with-docs', 'Build documentation (requires gtk-doc).'],
+  ]
+  end
+
+  def patches
+    # Patch out the fragile gtk-doc build unless user explicitly wants docs:
+    DATA
+    
     # Patches used in the official GTK+ OSX installer project:
     # http://git.gnome.org/browse/gtk-osx/tree/modulesets-stable/gtk-osx.modules
     p = { :p1 => %W[
@@ -39,9 +49,9 @@ class Gtkx < Formula
       http://git.gnome.org/browse/gtk-osx/plain/patches/0008-Bug-670373-gtk2-modules-printing-cups-gtkprintbackendcups.patch
       http://git.gnome.org/browse/gtk-osx/plain/patches/0009-Bug-670373-gtk2-modules-printing-cups-gtkprintbackendcups.patch
       ]}
+      p[:p0] = [ DATA ] unless ARGV.include? '--with-docs'
     return p
   end
-
 
   def install
     # We have to rerun autotools due to patches:
@@ -65,5 +75,84 @@ class Gtkx < Formula
   def test
     system "#{bin}/gtk-demo"
   end
-  
 end
+
+
+# Patches out docs due to the gtk-doc dependency. Gtk-doc requires the Python
+# libxml2 lib, which is an optional install and thus can't be depended upon.
+__END__
+diff --git Makefile.am Makefile.am
+index 8e3a2f1..fe3901b 100644
+--- Makefile.am
++++ Makefile.am
+@@ -2,7 +2,7 @@
+ include $(top_srcdir)/Makefile.decl
+ 
+ SRC_SUBDIRS = gdk gtk modules demos tests perf
+-SUBDIRS = po po-properties $(SRC_SUBDIRS) docs m4macros build
++SUBDIRS = po po-properties $(SRC_SUBDIRS) m4macros build
+ 
+ # require automake 1.4
+ AUTOMAKE_OPTIONS = 1.7
+diff --git configure.in configure.in
+index 24f1d32..010394c 100644
+--- configure.in
++++ configure.in
+@@ -1583,39 +1583,6 @@ fi
+ GOBJECT_INTROSPECTION_CHECK([0.9.3])
+ 
+ ##################################################
+-# Checks for gtk-doc and docbook-tools
+-##################################################
+-
+-GTK_DOC_CHECK([1.11])
+-
+-AC_CHECK_PROG(DB2HTML, db2html, true, false)
+-AM_CONDITIONAL(HAVE_DOCBOOK, $DB2HTML)
+-
+-AC_ARG_ENABLE(man,
+-              [AC_HELP_STRING([--enable-man],
+-                              [regenerate man pages from Docbook [default=no]])],enable_man=yes,
+-              enable_man=no)
+-
+-if test "${enable_man}" != no; then
+-  dnl
+-  dnl Check for xsltproc
+-  dnl
+-  AC_PATH_PROG([XSLTPROC], [xsltproc])
+-  if test -z "$XSLTPROC"; then
+-    enable_man=no
+-  fi
+-
+-  dnl check for DocBook DTD and stylesheets in the local catalog.
+-  JH_CHECK_XML_CATALOG([-//OASIS//DTD DocBook XML V4.1.2//EN],
+-     [DocBook XML DTD V4.1.2],,enable_man=no)
+-  JH_CHECK_XML_CATALOG([http://docbook.sourceforge.net/release/xsl/current/manpages/docbook.xsl],
+-     [DocBook XSL Stylesheets],,enable_man=no)
+-fi
+-
+-AM_CONDITIONAL(ENABLE_MAN, test x$enable_man != xno)
+-
+-
+-##################################################
+ # Output commands
+ ##################################################
+ 
+@@ -1717,16 +1684,6 @@ demos/Makefile
+ demos/gtk-demo/Makefile
+ demos/gtk-demo/geninclude.pl
+ tests/Makefile
+-docs/Makefile
+-docs/reference/Makefile
+-docs/reference/gdk/Makefile
+-docs/reference/gdk/version.xml
+-docs/reference/gtk/Makefile
+-docs/reference/gtk/version.xml
+-docs/reference/libgail-util/Makefile
+-docs/faq/Makefile
+-docs/tools/Makefile
+-docs/tutorial/Makefile
+ build/Makefile
+ build/win32/Makefile
+ build/win32/vs9/Makefile
+
