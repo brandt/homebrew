54a6d0e9153136373a686efff66eb9bd642d7ca2
J. Brandt Buckley <brandt@runlevel1.com>
2012-07-04 20:28:49 -0700

    Added patches from official OSX installer
    The official GTK+ OSX installer is a python app that downloads and
    applies a set of patches from gnome.org. I've included the patches
    specific to 2.24.9, the latest 2.x version it supports.
    
    These patches mostly concern Quartz integration and the new version of
    Cups in Mac OS X 10.8 (Mountain Lion).

--- Library/Formula/gtk+.rb
+++ Library/Formula/gtk+.rb
@@ -6,10 +6,9 @@ class Gtkx < Formula
   sha256 'ea56e31bb9d6e19ed2e8911f4c7ac493cb804431caa21cdcadae625d375a0e89'
 
   depends_on 'pkg-config' => :build
-  depends_on 'xz' => :build
+  depends_on 'automake' => :build
+  depends_on 'libtool' => :build
   depends_on 'glib'
-  depends_on 'jpeg'
-  depends_on 'libtiff'
   depends_on 'gdk-pixbuf'
   depends_on 'pango'
   depends_on 'jasper' => :optional
@@ -22,36 +21,49 @@ class Gtkx < Formula
     cause "Undefined symbols when linking"
   end
   
-  def options
-    [['--enable-cups', 'Enable CUPS support']]
+  def patches    
+    # Patches used in the official GTK+ OSX installer project:
+    # http://git.gnome.org/browse/gtk-osx/tree/modulesets-stable/gtk-osx.modules
+    p = { :p1 => %W[
+      http://git.gnome.org/browse/gtk-osx/plain/patches/0004-Bug-571582-GtkSelection-implementation-for-quartz.patch
+      http://git.gnome.org/browse/gtk-osx/plain/patches/0006-Bug-658722-Drag-and-Drop-sometimes-stops-working.patch
+      http://git.gnome.org/browse/gtk-osx/plain/patches/0008-Implement-GtkDragSourceOwner-pasteboardChangedOwner.patch
+      http://git.gnome.org/browse/gtk-osx/plain/patches/gtk+-Bug-655065-Better-Fix.patch
+      http://git.gnome.org/browse/gtk-osx/plain/patches/0001-Bug-670373-gtk2-modules-printing-cups-gtkprintbackendcups.patch
+      http://git.gnome.org/browse/gtk-osx/plain/patches/0002-gtk2-Extract-reasons-and-reasons_desc-arrays-to-file-leve.patch
+      http://git.gnome.org/browse/gtk-osx/plain/patches/0003-gtk2-Create-enum-PrinterStateLevel.patch
+      http://git.gnome.org/browse/gtk-osx/plain/patches/0004-gtk2-Extract-printer-setup-variables-into-a-struct.patch
+      http://git.gnome.org/browse/gtk-osx/plain/patches/0005-gtk2-Extract-function-cups_printer_handle_attribute.patch
+      http://git.gnome.org/browse/gtk-osx/plain/patches/0006-gtk2-Extract-Function-cups_create_printer.patch
+      http://git.gnome.org/browse/gtk-osx/plain/patches/0007-gtk2-Move-some-variable-declarations-into-the-scopes-in-w.patch
+      http://git.gnome.org/browse/gtk-osx/plain/patches/0008-Bug-670373-gtk2-modules-printing-cups-gtkprintbackendcups.patch
+      http://git.gnome.org/browse/gtk-osx/plain/patches/0009-Bug-670373-gtk2-modules-printing-cups-gtkprintbackendcups.patch
+      ]}
+    return p
   end
 
+
   def install
-    args = [ "--disable-debug",
-             "--disable-dependency-tracking",
-             "--prefix=#{prefix}",
-             "--disable-glibtest",
-             "--disable-introspection",
-             "--disable-visibility"]
+    # We have to rerun autotools due to patches:
+    ENV['LIBTOOLIZE'] = 'glibtoolize'
+    system "autoreconf", "--force", "--install"
 
-    # 2012-04-05: Optionally enable CUPS support which is broken in Mac OS X 10.8
-    args << '--disable-cups' unless ARGV.include? '--enable-cups'
-
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --disable-glibtest
+      --disable-introspection
+      --disable-visibility
+      --disable-maintainer-mode
+    ]
+    
     system "./configure", *args
     system "make install"
   end
-  
+
   def test
     system "#{bin}/gtk-demo"
   end
   
-  def caveats
-    return <<-EOS.undent
-    By default, this package is installed without CUPS support because it is broken
-    in recent versions of Mac OS X.  
-    
-    For CUPS support, reinstall with:
-     brew install --with-cups gtk+
-    EOS
-  end
 end
