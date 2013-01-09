f20f9f0d3fe281543c39c752dade017953ebb91e
J. Brandt Buckley <brandt@runlevel1.com>
2013-01-08 16:41:40 -0800

    Fixes issues compiling libnet with newer automake
    Issue:
    - Using the deprecated 'AM_CONFIG_HEADER' macro causes the build to fail with the current version of automake.
    - Using 'AC_CONFIG_MACRO_DIR([m4])' when the 'm4' directory doesn't exist also causes a failure.
    
    Remediation:
    - Adds a patch that swaps the old macro for the new one, 'AC_CONFIG_HEADERS'.
    - Makes the missing 'm4' directory in the build root before running autoreconf.

--- Library/Formula/libnet.rb
+++ Library/Formula/libnet.rb
@@ -10,14 +10,20 @@ class Libnet < Formula
   depends_on :autoconf
   depends_on :libtool
 
-  # Fix raw sockets support
+  # Patch 1: Fix raw sockets support
+  # Patch 2: Fixes deprecated m4 macro preventing libnet from being compiled with newer versions of automake
   def patches
-    {:p0 =>
-     "https://trac.macports.org/export/95336/trunk/dports/net/libnet11/files/patch-configure.in.diff"
-    }
+    { :p0 => [
+       "https://trac.macports.org/export/95336/trunk/dports/net/libnet11/files/patch-configure.in.diff",
+       "https://gist.github.com/raw/4479981/e6e37d367b8d82a251e39e5b1338527a22ce22b8/patch-deprecated-macro_configure.in.diff"
+    ]}
   end
 
   def install
+    # Necessary because autoreconf will fail on 'configure.in: AC_CONFIG_MACRO_DIR([m4])' if the dir doesn't exist
+    # This workaround can be removed when we no longer need to run autoreconf for the raw sockets support patch
+    mkdir_p 'm4'
+
     system "autoreconf --force --install"
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
