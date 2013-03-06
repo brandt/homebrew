df200adb6f606713f20fe18a1f6fb4c0740072b1
J. Brandt Buckley <brandt@runlevel1.com>
2013-03-06 07:56:32 -0800

    Added bfgminer stable release

--- Library/Formula/bfgminer.rb
+++ Library/Formula/bfgminer.rb
@@ -2,6 +2,8 @@ require 'formula'
 
 class Bfgminer < Formula
   homepage 'https://github.com/luke-jr/bfgminer'
+  url 'http://luke.dashjr.org/programs/bitcoin/files/bfgminer/2.99.1/bfgminer-2.99.1.zip'
+  sha1 '8e1e96ef3fd374fc88870ee9a501d0b39dda8cc7'
   head 'https://github.com/luke-jr/bfgminer.git', :branch => 'bfgminer'
 
   depends_on 'autoconf' => :build
